096408cdbfdabda058a85dc2c73f8f39578d9cfb
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-12 15:10:12 +0500

    _01_01 => print() with "sep" arg dn

---
+++ 01_fundamentals/01_basics.py
@@ -0,0 +1,8 @@
+# hello world | different types of print
+print("hello world")
+print("first", "second", "third", sep=' | ')  #by-default sep is comma, we can change like this
+
+# variables
+# comments
+# input_output
+# python keywords
