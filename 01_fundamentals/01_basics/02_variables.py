b5de21b36d0ee796e69178fadb487eb69f909294
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-13 13:09:51 +0500

    diffs naming convens of .py

--- 01_fundamentals/01_basics/02_variables.py
+++ 01_fundamentals/01_basics/02_variables.py
@@ -14,4 +14,16 @@ height = 6.1      #float num  [num with decimal regardless of +/-]
 equation = 12 + 7j      #complex num [this eqn type num, can only use "j", regardless of +/-]
 height = "6.2"      #updating height 'cause vars are dynamic in .py
 
-print(age, height, equation, sep='\n')
+# print(age, height, equation, sep='\n')
+
+'''================Multiple Assignment============'''
+x,y,z = 1,2,3           #can asgn mul vals like arr destructing in js
+# print(x,y,z)
+
+'''===========Naming conventions in .py=================='''
+camelCase = "avoid it for general use"
+snake_case = "recommended for general use"
+ALL_CAPS = "recommended for constants vals"
+_single_leading_underscore = "recommended to use _ in the beginning for private variables"
+
+# print(camelCase, snake_case, ALL_CAPS, _single_leading_underscore, sep='\n')
