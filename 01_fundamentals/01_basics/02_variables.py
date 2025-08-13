d20e7d48673d23d9202a0daa05bae56b51a864cb
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-13 12:58:51 +0500

    nums assgnd in vars

---
+++ 01_fundamentals/01_basics/02_variables.py
@@ -0,0 +1,17 @@
+'''
+01_numbers.py             # int, float, complex
+02_strings.py             # String creation, methods, formatting
+03_booleans.py            # True, False, logical operations
+04_type_conversion.py     # Casting between types
+05_constants.py           # Naming conventions for constants
+data_types_exercises.py   # Practice problems
+data_types_cheatsheet.md  # Quick reference
+'''
+
+'''=============Assigning nums variable ============'''
+age = 19    #int number [num without decimal regardless of +/-]
+height = 6.1      #float num  [num with decimal regardless of +/-]
+equation = 12 + 7j      #complex num [this eqn type num, can only use "j", regardless of +/-]
+height = "6.2"      #updating height 'cause vars are dynamic in .py
+
+print(age, height, equation, sep='\n')
