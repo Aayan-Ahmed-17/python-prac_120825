dc929222f353d71e0315632cdb174cc032473b44
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-18 15:21:44 +0500

    assign oprs dn

--- 01_fundamentals/03_operators/01_operators.py
+++ 01_fundamentals/03_operators/01_operators.py
@@ -35,11 +35,41 @@ COMPARISION OPERATORS
 '''
 LOGICAL OPERATORS
 '''
-print(True and True)               #f
-print(False and True)               #f   
-print(True and False)               #f   
-print(True or True)                  #T
-print(False or True)                  #T
-print(False or False)                  #F
-print(not True)                         #f
-print(not False)                        #T
+# print(True and True)               #f
+# print(False and True)               #f   
+# print(True and False)               #f   
+# print(True or True)                  #T
+# print(False or True)                  #T
+# print(False or False)                  #F
+# print(not True)                         #f
+# print(not False)                        #T
+
+'''
+ASSIGNMENT OPERATORS
+=   # Basic assignment
++=  # Add and assign
+-=  # Subtract and assign
+*=  # Multiply and assign
+/=  # Divide and assign
+//= # Floor divide and assign
+%=  # Modulus and assign
+**= # Exponent and assign
+'''   
+
+basic_opr =   2                     #simply assigning value
+print(basic_opr)
+basic_opr += 4                    #basic_opr = basic_opr + 4
+print(basic_opr)
+basic_opr -= 3                     # subtractin 3 from basic_opr (6)
+print(basic_opr)
+basic_opr *= 5                      #multiplyin 5 with 3
+print(basic_opr)
+basic_opr /= 3                      #dividin 3  by 15
+print(basic_opr)
+basic_opr //= 2                     # divide then floor to real num 
+print(basic_opr)
+basic_opr %= 1                     # divin modulus of 2 by 1 which is 0
+print(basic_opr)
+basic_opr **= 4                     # 0 ** 4
+print(basic_opr)
+
