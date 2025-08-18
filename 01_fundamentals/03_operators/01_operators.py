a234cddfff6c7dff9d6c4aa53bed51dd5c82640d
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-18 14:48:06 +0500

    arithematic oprs done

---
+++ 01_fundamentals/03_operators/01_operators.py
@@ -0,0 +1,22 @@
+'''
+ARITHMETIC OPERATORS
+'''
+
+a = int(input("enter your first number"))
+b = int(input("enter your second number"))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b 
+floor_division = a // b
+modulus = a % b
+expontentional = a ** b
+
+print(f"{a} + {b}: addition: {addition}" )
+print(f"{a} - {b}: subtraction: {subtraction}" )
+print(f"{a} * {b}: multiplication: {multiplication}" )
+print(f"{a} / {b}: division: {division}" )
+print(f"{a} // {b}: floor_division: {floor_division}" )
+print(f"{a} % {b}: modulus: {modulus}" )
+print(f"{a} ** {b}: expontentional: {expontentional}" )     
