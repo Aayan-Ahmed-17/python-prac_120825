7c3b128234c05d4cedefc075558eab99f73497ab
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-18 14:57:30 +0500

    compare b/w diff data types dn

--- 01_fundamentals/03_operators/01_operators.py
+++ 01_fundamentals/03_operators/01_operators.py
@@ -2,21 +2,32 @@
 ARITHMETIC OPERATORS
 '''
 
-a = int(input("enter your first number"))
-b = int(input("enter your second number"))
+# a = int(input("enter your first number"))
+# b = int(input("enter your second number"))
 
-addition = a + b
-subtraction = a - b
-multiplication = a * b
-division = a / b 
-floor_division = a // b
-modulus = a % b
-expontentional = a ** b
+# addition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b 
+# floor_division = a // b
+# modulus = a % b
+# expontentional = a ** b
 
-print(f"{a} + {b}: addition: {addition}" )
-print(f"{a} - {b}: subtraction: {subtraction}" )
-print(f"{a} * {b}: multiplication: {multiplication}" )
-print(f"{a} / {b}: division: {division}" )
-print(f"{a} // {b}: floor_division: {floor_division}" )
-print(f"{a} % {b}: modulus: {modulus}" )
-print(f"{a} ** {b}: expontentional: {expontentional}" )     
+# print(f"{a} + {b}: addition: {addition}" )
+# print(f"{a} - {b}: subtraction: {subtraction}" )
+# print(f"{a} * {b}: multiplication: {multiplication}" )
+# print(f"{a} / {b}: division: {division}" )
+# print(f"{a} // {b}: floor_division: {floor_division}" )
+# print(f"{a} % {b}: modulus: {modulus}" )
+# print(f"{a} ** {b}: expontentional: {expontentional}" )     
+
+
+'''
+COMPARISION OPERATORS
+'''
+print(1 == "1")               #false
+print(2 != 1)                   #true
+print(4 < 5 )                   #true
+print(True > False)         #true
+print("hi" <= "hello")     #false
+print("2" >= "2")            #true  | both side str needed if want to compare else print(2 >= "2") TypeError: '>=' not supported between instances of 'int' and 'str')
