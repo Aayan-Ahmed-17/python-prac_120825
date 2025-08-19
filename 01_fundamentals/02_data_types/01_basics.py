bd189e7d9efa81897980cd9ea95e1b6ac943490e
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-19 17:04:06 +0500

    01.02.01 ex02 dn

--- 01_fundamentals/02_data_types/01_basics.py
+++ 01_fundamentals/02_data_types/01_basics.py
@@ -13,3 +13,23 @@ values = [1, 2, True, False, "here", "three", "3"]
 
 # print(string, also_string, integer, float_num, is_available, values, sep='\n')
 # print(type(string), type(also_string), type(integer), type(float_num), type(is_available), type(values), sep='\n')
+
+'''ex02 arithematic opr
+# Given two numbers, perform all arithmetic operations
+a = 15
+b = 4
+# Calculate and print: addition, subtraction, multiplication, 
+# division, floor division, modulus, and exponentiation
+'''
+a = 15
+b = 4
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+floor_division= a // b
+modulus = a % b
+exponentiation = a ** b
+
+print(addition, subtraction, multiplication, division, floor_division, modulus, exponentiation, sep='\n')
