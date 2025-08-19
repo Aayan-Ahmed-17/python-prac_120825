94265772127c9d9db2eee27c172baa90a563163a
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-19 16:57:56 +0500

    01.02.01 ex01 dn

---
+++ 01_fundamentals/02_data_types/01_basics.py
@@ -0,0 +1,15 @@
+'''Exercise 1: Basic Data Types
+# Create variables of different types and print their types
+# Create: an integer, float, string, boolean, and list
+# Print each variable and its type using type() function
+'''
+
+string = "this is a string"
+also_string = "77"
+integer = 55
+float_num = 7.89
+is_available = True
+values = [1, 2, True, False, "here", "three", "3"]
+
+# print(string, also_string, integer, float_num, is_available, values, sep='\n')
+# print(type(string), type(also_string), type(integer), type(float_num), type(is_available), type(values), sep='\n')
