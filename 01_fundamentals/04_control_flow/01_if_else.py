8e5022c226e04a0b08142e1e91a4eb543cfd5d26
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-22 16:11:46 +0500

    chained comparision in if/else

--- 01_fundamentals/04_control_flow/01_if_else.py
+++ 01_fundamentals/04_control_flow/01_if_else.py
@@ -1,14 +1,29 @@
 age = 18
+x = 12
+y = 23
+z = 35
 
 # PYTHON - Uses indentation (4 spaces recommended)
-if age >= 18:
-    print("You are an adult")
-    print("You can vote")
-elif age >= 13:
-    print("You are a teenager")
-else:
-    print("You are a child")
+# if age >= 18:
+#     print("You are an adult")
+#     print("You can vote")
+# elif age >= 13:
+#     print("You are a teenager")
+# else:
+#     print("You are a child")
 
 # Single line if (ternary-like)
 status = "adult" if age >= 18 else "minor"
-print(status)
+# print(status)
+
+'''# Chained comparisons (PYTHON UNIQUE!)'''
+
+if y > x < z:                       #no need to type "and" keyword python speciality
+      print("running")
+
+if type(age) is int and len(status) >= 2:
+      print("All conditions true")
+
+else:
+      print(False)
+
