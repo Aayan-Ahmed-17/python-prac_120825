a783cd41ebad3a68c1bad8e85132bb2f4fdc161d
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-23 11:30:42 +0500

    match case with guards (if conditions)

--- 01_fundamentals/04_control_flow/02_match_statement.py
+++ 01_fundamentals/04_control_flow/02_match_statement.py
@@ -5,18 +5,28 @@
 
 day = 6
 
-match day:
-      case 1:
-            print("Monday")
-      case 2:
-            print("Tuesday")
-      case 3:
-            print("Wednesday")
-      case 4:
-            print("Thursday")
-      case 5:
-            print("Friday")
-      case 6:
-            print("Saturday")
-      case _:                                   # used as default case
-            print("Sunday")
+# match day:
+#       case 1:
+#             print("Monday")
+#       case 2:
+#             print("Tuesday")
+#       case 3:
+#             print("Wednesday")
+#       case 4:
+#             print("Thursday")
+#       case 5:
+#             print("Friday")
+#       case 6:
+#             print("Saturday")
+#       case _:                                   # used as default case
+#             print("Sunday")
+
+num = input("Enter your number here : ")
+
+match num:
+      case str() if int(num) > 0:                                 #Checks if the num is string if it is then this converts them into integer then compares them
+            print(f"Positive integer: {num}")
+      case str() if int(num) < 0:
+            print(f"Positive negative: {num}")
+      case str() if int(num) == 0:
+            print(f"Neither positive nor negative: {num}")
---
+++ 01_fundamentals/05_loops/for_loop.py
@@ -0,0 +1,3 @@
+'''
+Not uses for loop syntax as js do instead uses for-in loop of js kind of syntax
+'''
