463db3ce9ec68828cf482104d6ef01d7bb7eb186
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-23 11:41:54 +0500

    01.04.02 ex01 dn

--- 01_fundamentals/04_control_flow/02_match_statement.py
+++ 01_fundamentals/04_control_flow/02_match_statement.py
@@ -4,7 +4,6 @@
 '''
 
 day = 6
-
 # match day:
 #       case 1:
 #             print("Monday")
@@ -21,12 +20,27 @@ day = 6
 #       case _:                                   # used as default case
 #             print("Sunday")
 
-num = input("Enter your number here : ")
 
-match num:
-      case str() if int(num) > 0:                                 #Checks if the num is string if it is then this converts them into integer then compares them
-            print(f"Positive integer: {num}")
-      case str() if int(num) < 0:
-            print(f"Positive negative: {num}")
-      case str() if int(num) == 0:
-            print(f"Neither positive nor negative: {num}")
+# num = input("Enter your number here : ")
+# match num:
+#       case str() if int(num) > 0:                                 #Checks if the num is string if it is then this converts them into integer then compares them
+#             print(f"Positive integer: {num}")
+#       case str() if int(num) < 0:
+#             print(f"Positive negative: {num}")
+#       case str() if int(num) == 0:
+#             print(f"Neither positive nor negative: {num}")
+
+'''ex01 Basic Conditionals
+Write a program that takes a number and prints whether it's positive, negative, or zero.
+'''
+
+user_num = int(input("Enter your number here : "))
+match user_num:
+      case 0:
+            print(f"Neither negative nor positive integer: {user_num}")
+      case _ if user_num > 0:
+            print(f"Positive integer: {user_num}")
+      case _ if user_num < 0:
+            print(f"Negative integer: {user_num}")
+      case _ :
+            print(f"Invalid input: {user_num}")
