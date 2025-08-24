8cabaa7c7269f2091aed0a3e1cf5675edf5bb224
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-24 17:35:25 +0500

    iterate through range of numbers with steps (third arg of range()

--- 01_fundamentals/05_loops/01_for_loop.py
+++ 01_fundamentals/05_loops/01_for_loop.py
@@ -10,3 +10,5 @@ fruits = ["apple", "banana", "pineapple"]                   #list / array of fru
 # for i in range(2, 10):                                #if one arg in range(n) then will go "0" to "n" if two args in range(n1, n2) then will go "n1" to "n2"
 #       print(f"i => {i}")
       
+# for i in range(0, 14, 3):                             #starting from 0 going to 14 and skipping two nums after each third number [0, 3, 6, 9 ... 12]
+#       print(f"hello {i}")
