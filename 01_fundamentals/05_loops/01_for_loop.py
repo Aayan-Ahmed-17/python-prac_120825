38050e4b3b044cc8d11021c054404b9351824cec
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-24 17:32:43 +0500

    iterate through range of numbers

--- 01_fundamentals/05_loops/01_for_loop.py
+++ 01_fundamentals/05_loops/01_for_loop.py
@@ -3,5 +3,10 @@ Not uses for loop syntax as js do instead uses for-in loop of js kind of syntax
 '''
 
 fruits = ["apple", "banana", "pineapple"]                   #list / array of fruits
-for fruit in fruits:
-      print(fruit)
+# for fruit in fruits:
+#       print(fruit)
+      
+'''iterate over range'''
+# for i in range(2, 10):                                #if one arg in range(n) then will go "0" to "n" if two args in range(n1, n2) then will go "n1" to "n2"
+#       print(f"i => {i}")
+      
