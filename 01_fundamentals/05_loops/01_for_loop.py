a0a09116e12b6b7e16c0e3b140b7ae9fb6b0fc8c
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-24 18:08:54 +0500

    for loop with dictionary | better conention | checked types

--- 01_fundamentals/05_loops/01_for_loop.py
+++ 01_fundamentals/05_loops/01_for_loop.py
@@ -25,3 +25,20 @@ fruits = ["apple", "banana", "pineapple"]                   #list / array of fru
 # my_name = "Aayan Ahmed Tejani"
 # for i, char in enumerate(my_name, start=1):
 #       print(f'Letters in my name {i} => {char}')
+
+'''iterate over dictionary / obj with for in loop'''
+person = {"name": "Ahmed", "age": 19, "isStudent": True}
+for key in person:
+      print(f"{key}: {person[key]}")           
+
+'''better approach of iteration for dictionary'''
+for key, value in person.items():
+      print(i, key)
+      
+for elem in person.items():
+      for sub_elem in elem:
+            print(sub_elem)
+
+print(person.items())
+print(person)
+print(type(person), type(person.items()))
