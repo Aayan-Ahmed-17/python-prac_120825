68604d4be89c48a9b1799260abd76f571aec9728
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-26 10:18:01 +0500

    while with else keyword special in py

--- 01_fundamentals/05_loops/02_while_loop.py
+++ 01_fundamentals/05_loops/02_while_loop.py
@@ -1,4 +1,12 @@
-count = 0
-while count <= 5:
-      print(count)
-      count += 1                    #No ++ operator in py
+# count = 0
+# while count <= 5:
+#       print(count)
+#       count += 1                    #No ++ operator in py
+      
+'''While with else (unique to Python!)'''
+count = 4
+while count >= 0:
+      print(f"count = {count}")
+      count -= 1
+else:
+      print("While loop ended successfully")                #runs when loop wasn't broken
