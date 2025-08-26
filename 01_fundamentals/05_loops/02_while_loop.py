754959ff7843461f6bdb3ee9f1babf64960c7299
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-26 10:24:00 +0500

    while loop break n continue keywords

--- 01_fundamentals/05_loops/02_while_loop.py
+++ 01_fundamentals/05_loops/02_while_loop.py
@@ -4,9 +4,22 @@
 #       count += 1                    #No ++ operator in py
       
 '''While with else (unique to Python!)'''
-count = 4
-while count >= 0:
-      print(f"count = {count}")
-      count -= 1
-else:
-      print("While loop ended successfully")                #runs when loop wasn't broken
+# count = 4
+# while count >= 0:
+#       print(f"count = {count}")
+#       count -= 1
+# else:
+#       print("While loop ended successfully")                #runs when loop wasn't broken
+      
+      
+'''while loop with break n cont, if n elif'''
+num = 20
+while num >= 0:
+      num -= 1
+      if num == 4:
+            break
+      elif num % 2 == 0:
+            continue
+      else:
+            print(num)
+      
