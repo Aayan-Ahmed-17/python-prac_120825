c57c5f40d5a4d17e8345941c7ba127def03ca035
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-26 16:05:50 +0500

    while loop with complex condtion

--- 01_fundamentals/05_loops/02_while_loop.py
+++ 01_fundamentals/05_loops/02_while_loop.py
@@ -13,13 +13,25 @@
       
       
 '''while loop with break n cont, if n elif'''
-num = 20
-while num >= 0:
-      num -= 1
-      if num == 4:
-            break
-      elif num % 2 == 0:
-            continue
-      else:
-            print(num)
+# num = 20
+# while num >= 0:
+#       num -= 1
+#       if num == 4:
+#             break
+#       elif num % 2 == 0:
+#             continue
+#       else:
+#             print(num)
       
+'''while loop with complex condition'''
+import random
+attempts = 0
+max_attempts = 5
+while attempts < max_attempts and random.random() > 0.3:
+    print(f"Attempt {attempts + 1}")
+    attempts += 1
+else:
+    if attempts == max_attempts:
+        print("Max attempts reached")
+    else:
+        print("Condition met")
