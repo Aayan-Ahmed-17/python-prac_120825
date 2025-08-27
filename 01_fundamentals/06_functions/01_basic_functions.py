f5cd998f5f7aee008115d6fc7036b5691b20c778
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-08-27 13:59:04 +0500

    func with param dn

--- 01_fundamentals/06_functions/01_basic_functions.py
+++ 01_fundamentals/06_functions/01_basic_functions.py
@@ -1,6 +1,13 @@
 # Basic function definition
-def greet():
-      '''docstring comment, used to indicate purpose of a func'''
-      print("Hello, Dev")
+# def greet():
+#       '''docstring comment, used to indicate purpose of a func'''
+#       print("Hello, Dev")
 
-greet()     #function call
+# greet()     #function call
+
+def greet_name(name):
+      '''greets with capitalized name'''
+      print(f"Hello! Sir {name.capitalize()}")
+      
+
+greet_name("aayan ahmed")
