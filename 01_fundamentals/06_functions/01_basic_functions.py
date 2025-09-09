d3d9f66eb247409c56251f7842ed158662bc81a1
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-09-09 14:32:02 +0500

    01.06.01 ex01 dn

--- 01_fundamentals/06_functions/01_basic_functions.py
+++ 01_fundamentals/06_functions/01_basic_functions.py
@@ -26,7 +26,15 @@
 '''ex01
 Create a function called calculate_area that takes length and width as arguments. The width argument should have a default value of 10. The function should return the calculated area.'''
 
-def calculate_area(len, width = 10):
-      return len * width
+# def calculate_area(len, width = 10):
+#       return len * width
 
-print(calculate_area(4, 2))
+# print(calculate_area(4, 2))
+'''ex02
+Write a function called get_user_info that takes a first_name and last_name as arguments and returns a tuple containing the full name and the length of the full name. For example, for "John" and "Doe", it should return ("John Doe", 8).'''
+
+def get_user_info(f_name, last_name):
+      full_name = f"{f_name} {last_name}"
+      return (full_name, len(full_name))
+
+print(get_user_info("Aayan", "Ahmed"))
