e5fe15566916028872944360bfd7269628ea20b9
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 17:50:17 +0500

    num_guess_game smit "rules dfned"

---
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -0,0 +1,7 @@
+#Number guessing game
+'''
+1) user can give max n min range
+2) user has limited chances based on the difficulty level EASY (more chance), MEDIUM (less chance, max rndm range), HARD(least chance, max rndm range)
+
+'''
+
