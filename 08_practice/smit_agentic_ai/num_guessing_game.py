a4f7ca52c2d25c264e36eedcb2ee96d4309c8e3f
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 20:24:23 +0500

    rndm num | user_guess took n printed

--- 08_practice/smit_agentic_ai/num_guessing_game.py
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -1,21 +1,32 @@
+import random
+
 #Number guessing game
 '''
 1) user can give max n min range
 2) user has limited chances based on the difficulty level EASY (more chance), MEDIUM (less chance, max rndm range), HARD(least chance, max rndm range)
 
 '''
-user_select = input("Choose 0-2 for easy-hard = ")
-difficulty_levels = ["EASY", "MEDIUM", "HARD"]
-rules_dict = {
-      "EASY": {"chance": 5, "range": 5},
-      "MEDIUM": {"chance": 3, "range": 15},
-      "HARD": {"chance": 2, "range": 15}
-}
-print(("USER SELECTED", user_select))
+# user_select = input("Choose 0-2 for easy-hard = ")
+# difficulty_levels = ["EASY", "MEDIUM", "HARD"]
+# rules_dict = {
+#       "EASY": {"chance": 5, "range": 5},
+#       "MEDIUM": {"chance": 3, "range": 15},
+#       "HARD": {"chance": 2, "range": 15}
+# }
+# print(("USER SELECTED", user_select))
 
-if user_select.isdigit() and  (0 <= int(user_select) <= 2):
-      user_integer = int(user_select)
-      print(rules_dict[difficulty_levels[user_integer]])
-      '''rest of the code'''
-else:
-      print("not an int")
+# if user_select.isdigit() and  (0 <= int(user_select) <= 2):
+#       user_integer = int(user_select)
+#       print(rules_dict[difficulty_levels[user_integer]])
+#       '''rest of the code'''
+# else:
+#       print("not an int")
+
+
+
+'''guess rndm num game logic'''
+
+rndm_num = random.randint(1, 100)
+user_guess = int(input("Guess ur number (1-100):   "))
+
+print(f"rndm_num: {rndm_num} and ur guess is: {user_guess}")
