010bbbd19c8b36366e2fe41394055b372350cb43
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 20:13:51 +0500

    validate should by num data type N 0-2

--- 08_practice/smit_agentic_ai/num_guessing_game.py
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -6,27 +6,26 @@
 '''
 user_select = input("Choose 0-2 for easy-hard = ")
 difficulty_levels = ["EASY", "MEDIUM", "HARD"]
-
+rules_dict = {
+      "EASY": {
+            "chance": 5,
+            "range": 5
+            },
+      "MEDIUM": {
+            "chance": 3,
+            "range": 15
+            },
+      "HARD": {
+            "chance": 2,
+            "range": 15
+            }
+}
 print(("USER SELECTED", user_select))
 
-if user_select.isdigit():
+if user_select.isdigit() and  (0 <= int(user_select) <= 2):
+      user_integer = int(user_select)
       print("yes it is int")
+      print(rules_dict[difficulty_levels[user_integer]])
       '''rest of the code'''
 else:
       print("not an int")
-# rules_dict = {
-#       "EASY": {
-#             "chance": 5,
-#             "range": 5
-#             },
-#       "MEDIUM": {
-#             "chance": 3,
-#             "range": 15
-#             },
-#       "HARD": {
-#             "chance": 2,
-#             "range": 15
-#             }
-# }
-
-# print(rules_dict[difficulty_levels[user_select]])
