f4a66323f6605570a55882dea917f15599fb6027
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 17:59:55 +0500

    basic structure dn

--- 01_fundamentals/02_data_types/02_intermediate.py
+++ 01_fundamentals/02_data_types/02_intermediate.py
@@ -9,8 +9,10 @@ int_num = 89
 
 """Going to explore every single method of a list in python"""
 sample_list = ["Hello", 2, 2.5, True, False, "World"]
+# sample_list.append(["new", "elelm"])
+sample_list.extend("iterate example")           #puts every char / elem as a seperate child of a list
 print(sample_list)
 
 sample_num_list = [1, 2, 3, 4, 5, 2, 3, 4, 7, 1, 1, 1.02, 1.3, 2, 3, 4, 5, 6]
-sample_num_list.sort()
-print(sample_num_list)
+# sample_num_list.sort()
+# print(sample_num_list)
--- 08_practice/smit_agentic_ai/num_guessing_game.py
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -4,4 +4,23 @@
 2) user has limited chances based on the difficulty level EASY (more chance), MEDIUM (less chance, max rndm range), HARD(least chance, max rndm range)
 
 '''
+difficulty_levels = ["EASY", "MEDIUM", "HARD"]
 
+user_select = int(input("Choose 0-2 for easy-hard = "))
+print(("USER SELECTED", user_select))
+rules_dict = {
+      "EASY": {
+            "chance": 5,
+            "range": 5
+            },
+      "MEDIUM": {
+            "chance": 3,
+            "range": 15
+            },
+      "HARD": {
+            "chance": 2,
+            "range": 15
+            }
+}
+
+print(rules_dict[difficulty_levels[user_select]])
