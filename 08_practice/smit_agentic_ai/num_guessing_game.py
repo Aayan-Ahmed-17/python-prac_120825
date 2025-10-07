1de965db608582f8e76596edbd3d08d44af25111
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 20:04:35 +0500

    temp commit N if else implement to check wether int or not

--- 08_practice/smit_agentic_ai/num_guessing_game.py
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -4,23 +4,29 @@
 2) user has limited chances based on the difficulty level EASY (more chance), MEDIUM (less chance, max rndm range), HARD(least chance, max rndm range)
 
 '''
+user_select = input("Choose 0-2 for easy-hard = ")
 difficulty_levels = ["EASY", "MEDIUM", "HARD"]
 
-user_select = int(input("Choose 0-2 for easy-hard = "))
 print(("USER SELECTED", user_select))
-rules_dict = {
-      "EASY": {
-            "chance": 5,
-            "range": 5
-            },
-      "MEDIUM": {
-            "chance": 3,
-            "range": 15
-            },
-      "HARD": {
-            "chance": 2,
-            "range": 15
-            }
-}
 
-print(rules_dict[difficulty_levels[user_select]])
+if user_select.isdigit():
+      print("yes it is int")
+      '''rest of the code'''
+else:
+      print("not an int")
+# rules_dict = {
+#       "EASY": {
+#             "chance": 5,
+#             "range": 5
+#             },
+#       "MEDIUM": {
+#             "chance": 3,
+#             "range": 15
+#             },
+#       "HARD": {
+#             "chance": 2,
+#             "range": 15
+#             }
+# }
+
+# print(rules_dict[difficulty_levels[user_select]])
