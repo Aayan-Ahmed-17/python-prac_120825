17931045dac4b896fb0a4d37e334f0842f885b83
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-07 20:17:31 +0500

    unnecessary things rmvd

--- 08_practice/smit_agentic_ai/num_guessing_game.py
+++ 08_practice/smit_agentic_ai/num_guessing_game.py
@@ -7,24 +7,14 @@
 user_select = input("Choose 0-2 for easy-hard = ")
 difficulty_levels = ["EASY", "MEDIUM", "HARD"]
 rules_dict = {
-      "EASY": {
-            "chance": 5,
-            "range": 5
-            },
-      "MEDIUM": {
-            "chance": 3,
-            "range": 15
-            },
-      "HARD": {
-            "chance": 2,
-            "range": 15
-            }
+      "EASY": {"chance": 5, "range": 5},
+      "MEDIUM": {"chance": 3, "range": 15},
+      "HARD": {"chance": 2, "range": 15}
 }
 print(("USER SELECTED", user_select))
 
 if user_select.isdigit() and  (0 <= int(user_select) <= 2):
       user_integer = int(user_select)
-      print("yes it is int")
       print(rules_dict[difficulty_levels[user_integer]])
       '''rest of the code'''
 else:
