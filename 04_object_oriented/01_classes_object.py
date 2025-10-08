c5bd74c486992b07e4629f130019e066e7aef536
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-08 19:47:46 +0500

    temp commit

---
+++ 04_object_oriented/01_classes_object.py
@@ -0,0 +1,13 @@
+'''Going to start py oop concept from very basics'''
+
+#Create class
+class Car:
+      standand = "Elite Class"
+
+simple_obj = Car()
+
+print(simple_obj)
+
+lst = [1,2,3,4,5,5]
+lst.append(7)
+print(lst)
