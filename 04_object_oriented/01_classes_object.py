39f78d45ae36f5f58b6eda25e01a4b1559097d44
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-08 21:07:07 +0500

    access n manipulate prop dn

--- 04_object_oriented/01_classes_object.py
+++ 04_object_oriented/01_classes_object.py
@@ -1,13 +1,13 @@
 '''Going to start py oop concept from very basics'''
 
-#Create class
+'''Create class'''
 class Car:
       standand = "Elite Class"      #class attribute
 
 simple_obj = Car()
 # print(simple_obj)
 
-#Class with class attr, init(), instance attr
+'''Class with class attr, init(), instance attr'''
 class Mobile:
       brand = "Samsung"      #class attr | it's not imp | shared by all instances created by this class
       
@@ -18,5 +18,13 @@ class Mobile:
             self.in_stock = in_stock
             
 mobile1 = Mobile("S 25 ultra", 500000, True, True)
-print(mobile1)
-print(Mobile)
+# print(mobile1)
+# print(Mobile)
+
+'''Access props N manipulate them also'''
+print(mobile1.model_name, mobile1.brand ,sep='\n')
+
+mobile1.price = 600000
+print(mobile1.model_name, mobile1.brand, mobile1.price ,sep='\n')
+
+
