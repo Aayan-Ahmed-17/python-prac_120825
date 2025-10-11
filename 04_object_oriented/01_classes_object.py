75ee1daa745530f3a1ca4355a039201439c0c102
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-11 17:00:58 +0500

    04_01_01 dn | website code when don’t need to store data |my code when modeling real-world entities/ storing attributes.

--- 04_object_oriented/01_classes_object.py
+++ 04_object_oriented/01_classes_object.py
@@ -22,9 +22,34 @@ mobile1 = Mobile("S 25 ultra", 500000, True, True)
 # print(Mobile)
 
 '''Access props N manipulate them also'''
-print(mobile1.model_name, mobile1.brand ,sep='\n')
+# print(mobile1.model_name, mobile1.brand ,sep='\n')
 
 mobile1.price = 600000
-print(mobile1.model_name, mobile1.brand, mobile1.price ,sep='\n')
+# print(mobile1.model_name, mobile1.brand, mobile1.price ,sep='\n')
+
+'''============================Exercises questions=========================='''
+
+'''Exercise 1: Create a class Greeter with a method greet(name) that prints a greeting for the provided name.
+'''
+
+#my code
+class Greeter:
+      def __init__(self, name):
+            self.name = name
+            
+      def greet(self):
+            return f"Hello {self.name}"
+
+user1 = Greeter("Aayan Ahmed")
+print(user1.greet())
+
+#greeek for greek code
+class Greeter:
+    def greet(self, name):
+        print(f"Hello, {name}!")
+
+# Example usage:
+g = Greeter()
+g.greet("Gabriel")
 
 
