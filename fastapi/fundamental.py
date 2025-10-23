e1487404a0ab27ff00aab91a044db1fb4e85ad2a
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-23 17:32:39 +0500

    temp commit ex 03 not cmltd yet

--- fastapi/fundamental.py
+++ fastapi/fundamental.py
@@ -12,6 +12,19 @@ def root():
 
 """ex02: Greet User: Create an endpoint /greet/{name} that returns a personalized greeting with the name from the path parameter
 """
-@app.get('/greet/{name}')
+
+
+@app.get("/greet/{name}")
 def greetUser(name):
     return {"greet": f"Hello {name}"}
+
+
+"""ex03: Calculator Basic: Build endpoints for basic math operations:
+/add?a=5&b=3 → returns sum
+/subtract?a=10&b=4 → returns difference
+"""
+
+
+@app.get("/add/{num1}/{num2}")
+def sum_two_num(num1: int, num2: int):
+    return {"sum_result": f"the sum of {num1} & {num2} is {num1 + num2}"}
