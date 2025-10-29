8f0eef51e42bb1b36fbeb9e06b99855492ca5c74
Aayan Ahmed <aayanahmedtejani@gmail.com>
2025-10-29 19:56:24 +0500

    03.01.02 part 3 dn | iterate method

--- 03_intermediate/01_file_handling.py
+++ 03_intermediate/01_file_handling.py
@@ -19,13 +19,59 @@ ex02: Create a program that counts the number of lines in a text file.
 Skills tested: readlines() or iteration, basic file operations
 """
 
-#using readlines()
-def count_files_with_readlines(filepath):
+
+# using readlines()
+def count_lines_with_readlines(filepath):
     """
     Counts the number of lines in a file using the readlines() method.
     """
     try:
+        with open(filepath, "r") as file:
+            content = file.readlines()
+            return len(content)
+    except:
+        raise (f"File {filepath} not found")
+
+
+file_name = "dummy.txt"
+total_lines = count_lines_with_readlines(file_name)
+
+# print(f"File: {file_name}")
+# print(f"Total number of lines (readlines() Method): **{total_lines}**")
+
+
+# using readline()
+def count_lines_readline(filepath):
+    """
+    Counts the number of lines in a file using readline mthd.
+    """
+    total_lines = 0
+    try:
+        with open(filepath, "r") as file:
+            line = file.readline()
+            while line:
+                line = file.readline()
+                total_lines += 1
+            print(total_lines)
+    except:
+        print("An exception occurred")
+
+
+# count_lines_readline("dummy.txt")
+
+#using iteration method | recommended
+def count_lines_efficiently(filepath):
+    """
+    Counts the number of lines in a file by iterating over the file object.
+    """
+    line_count = 0
+    try:
       with open(filepath, 'r') as file:
-         content = file.readlines()
+          for line in file:
+              line_count += 1
+          
+          print(line_count)
     except:
       print('An exception occurred')
+
+count_lines_efficiently("dummy.txt")
